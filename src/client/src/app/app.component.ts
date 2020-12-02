a480c63e30c9866fe5190756abb36a573b047173
omlbucket <omelianai@gmail.com>
2020-12-02 22:16:55 +0200

    Added angular bootstrap, http client service. Added https support with trusted certificate. Fixed CORS for the API. Added gitignore for both API project and client project.

--- src/client/src/app/app.component.ts
+++ src/client/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  title = 'The pets daiting app';
+  pets: any;
+  // Dependency injection
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getPets();
+  }
+
+  getPets() {
+    this.http.get('https://localhost:5001/api/pets').subscribe(response => {
+      this.pets = response;
+    }, error => {
+      console.log(error);
+    });
+  }
 }
