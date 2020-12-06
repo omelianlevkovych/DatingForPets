1a10cf96fd2f3d76781053221279c95ac7c50536
omlbucket <omelianai@gmail.com>
2020-12-06 11:13:04 +0200

    Fixing extensions contain an emphasized items

--- src/API/Extensions/ApplicationServiceExtensions.cs
+++ src/API/Extensions/ApplicationServiceExtensions.cs
@@ -1,7 +1,34 @@
-namespace src.API.Extensions
+using API.Data;
+using API.Interfaces;
+using API.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace API.Extensions
 {
-    public class ApplicationServiceExtensions
-    {
-        
-    }
+  /// <summary>
+  /// The extension methods for application service.
+  /// </summary>
+  public static class ApplicationServiceExtensions
+  {
+        /// <summary>
+        /// Adds the application services collection.
+        /// </summary>
+        /// <param name="services">The service collection.</param>
+        /// <param name="config">The configuration.</param>
+        /// <returns>A <see cref="Task"/> representing the result of the asynchronous operation.</returns>
+        public static IServiceCollection AddApplicationServices(
+            this IServiceCollection services,
+            IConfiguration config)
+        {
+            services.AddScoped<ITokenService, TokenService>();
+            services.AddDbContext<DataContext>(options =>
+            {
+                options.UseSqlite(config.GetConnectionString("DefaultConnection"));
+            });
+
+            return services;
+        }
+  }
 }
