c3e9359448071aaaba78314327fbc8d53f12d557
omlbucket <omelianai@gmail.com>
2020-12-12 11:48:26 +0200

    Added components

--- src/client/src/app/app-routing.module.ts
+++ src/client/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MemberListComponent } from './members/member-list/member-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'members', component: MemberListComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/client/src/app/app.module.ts
+++ src/client/src/app/app.module.ts
@@ -5,10 +5,18 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MemberListComponent,
+    MemberDetailComponent,
+    ListsComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
