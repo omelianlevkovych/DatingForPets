03ea7f6c579573ad5d7064bc3d6dd9403c3a9239
Borys <borys@DESKTOP-39EO80E.localdomain>
2020-12-18 11:21:28 +0200

    removed unsafe trust all certificates line

--- scripts/data_manipulation_scripts/main.ps1
+++ scripts/data_manipulation_scripts/main.ps1
@@ -5,19 +5,6 @@
 
 Set-StrictMode -version 3.0
 
-# Ignore self-signed certificates
-add-type @'
-    using System.Net;
-    using System.Security.Cryptography.X509Certificates;
-    public class TrustAllCertsPolicy : ICertificatePolicy {
-        public bool CheckValidationResult(
-            ServicePoint srvPoint, X509Certificate certificate,
-            WebRequest request, int certificateProblem) {
-            return true;
-        }
-    }
-'@
-[System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
 
 while($true)
 {
